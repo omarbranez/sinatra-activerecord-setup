9da71ec11bd3ce2c5b4a0f9ac2830c728c62d134
Omar Branez <omar.branez@gmail.com>
2021-04-20 13:27:00 -0400

    commit tree 6801735ab6043ec027009e2581eb38d2276c5fb5

---
+++ db/migrate/20210420171827_create_dogs.rb
@@ -0,0 +1,17 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  
+  def up 
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+  def down
+    drop_table :dogs
+  end
+
+end
+
+# rake db:migrate SINATRA_ENV=development 
+# use development environment for shotgun and testing
